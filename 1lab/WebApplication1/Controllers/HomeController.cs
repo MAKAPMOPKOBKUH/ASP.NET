ba66cebdf7f5badf1e41741b31281de897896e5b
MAKAPMOPKOBKUH <wyezan@mail.ru>
2016-12-08 11:57:16 +0500

    1lab
    Patrialview

---
+++ 1lab/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Departments;
+
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            department Model = new department();
+            department dep0 = new department();
+            department dep1 = new department();
+            department dep00 = new department();
+            dep00.departments = new List<department>();
+
+            department dep11 = new department();
+            dep11.departments = new List<department>();
+
+            dep0.departments = new List<department>();
+            dep0.departments.Add(dep00);
+
+            dep1.departments = new List<department>();
+            dep1.departments.Add(dep11);
+
+            Model.departments = new List<department>();
+
+            Model.departments.Add(dep0);
+            Model.departments.Add(dep1);
+
+            return View(Model);
+        }
+
+        
+    }
+}
---
+++ 1lab/WebApplication1/Models/Departments.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Departments
+{
+    public class department
+    {
+        public string name { get; set; }
+        public int amount { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public List<employee> employees { get; set; }
+        public List<department> departments { get; set; }
+
+           public department()
+        {
+            name = "Department";
+            phone = "8942342374";
+            amount = 3;
+            employees = new List<employee>();
+            for (int i = 0; i < 3; i++)
+            {
+
+                employee emp = new employee();
+                emp.name = "employee" + i;
+                emp.age = 18 + i;
+                emp.phone = "89084327148";
+
+                employees.Add(emp);
+            }
+        }
+
+    };
+
+
+
+    public class employee {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string phone { get; set; }
+        
+
+
+       
+    
+    };
+}
+
+/*Для каждого отдела в классе-модели хранится следующая информация:
+название отдела (строка)
+количество сотрудников объектов (число)
+адрес офиса (строка)
+телефон офиса (строка)
+Для каждого сотрудника хранится следующая информация:
+ФИО сотрудника (строка)
+возраст (число)
+телефон сотрудника (строка)*/
