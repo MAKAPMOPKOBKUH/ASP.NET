99287bf001f11505cdd5cbbb8f29b9d3fb4805fc
MAKAPMOPKOBKUH <wyezan@mail.ru>
2017-01-06 21:10:03 +0500

    Аутентификация через вк

---
+++ 2lab/WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+using WebApplication5.Controllers;
+using VkNet.Enums.Filters;
+using System.Web.Script.Serialization;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            UserData Model = new UserData();
+
+
+            string code = Request["code"];
+
+        
+
+            if (code != null)
+            {
+
+               
+
+                string URI = String.Format("https://oauth.vk.com/access_token?client_id=5760826&client_secret=R8DEEzwLmgziPepArWVy&redirect_uri=http://localhost:1501/Home/About&code=" + code);
+               
+
+                var response = Load(URI);
+                var accessToken = DeserializeJson<AccessToken>(response);
+
+
+                URI = String.Format(
+                        "https://api.vk.com/method/users.get?uids={0}&fields=uid,first_name,last_name,photo_50",
+                        accessToken.user_id);
+
+                response = Load(URI);
+                var usersData = DeserializeJson<UsersData>(response);
+                Model = usersData.response.First();
+              
+
+            }
+
+            return View(Model);
+           
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public static string Load(string address)
+        {
+            var request = WebRequest.Create(address) as HttpWebRequest;
+            using (var response = request.GetResponse() as HttpWebResponse)
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public static T DeserializeJson<T>(string input)
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<T>(input);
+        }
+    }
+}
---
+++ 2lab/WebApplication5/Models/AccountViewModels.cs
@@ -0,0 +1,99 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication5.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+    }
+
+    public class ManageUserViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Текущий пароль")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение нового пароля")]
+        [Compare("NewPassword", ErrorMessage = "Новый пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("Password", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class AccessToken
+    {
+        public string access_token = null;
+        public string user_id = null;
+    }
+    public class user
+    {
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+
+        public string id { get; set; }
+
+        public string password { get; set; }
+        public string email { get; set; }
+
+    }
+
+    class UsersData
+    {
+        public UserData[] response = null;
+    }
+
+    public class UserData
+    {
+        public string uid = null;
+        public string first_name = null;
+        public string last_name = null;
+        public string photo_50 = null;
+    }
+ 
+
+  
+}
